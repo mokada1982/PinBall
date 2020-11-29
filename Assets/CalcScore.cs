f0097a21f14cf45b5bf93bd724f74cef302d014f
mokada1982 <m27seiun@gmail.com>
2020-11-29 23:24:25 +0900

    Initial commit

---
+++ Assets/CalcScore.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//--------------------------------------------------
+//追加
+//--------------------------------------------------
+using UnityEngine.UI;
+
+public class CalcScore : MonoBehaviour
+{
+  //Score
+  private int scoreVal;
+
+  //Scoreを表示するテキスト
+  private GameObject scoreText;
+
+  //--------------------------------------------------
+  // Start is called before the first frame update
+  //--------------------------------------------------
+  void Start()
+  {
+    //scoreの値を0にリセット
+    scoreVal = 0;
+
+    //シーン中のScoreTextオブジェクトを取得
+    this.scoreText = GameObject.Find("ScoreText");
+  }
+
+  //--------------------------------------------------
+  // Update is called once per frame
+  //--------------------------------------------------
+  void Update()
+  {
+    //scoreを表示
+    this.scoreText.GetComponent<Text>().text = scoreVal.ToString() + "点";
+  }
+
+  //--------------------------------------------------
+  //衝突時に呼ばれる関数
+  //--------------------------------------------------
+  void OnCollisionEnter(Collision other)
+  {
+    //衝突相手のタグを取得
+    string nameStr = other.gameObject.tag;
+
+    if (nameStr == "SmallStarTag")
+    {
+      scoreVal += 10;
+    }
+    else if (nameStr == "LargeStarTag")
+    {
+      scoreVal += 20;
+    }
+    else if (nameStr == "SmallCloudTag")
+    {
+      scoreVal += 50;
+    }
+    else if (nameStr == "LargeCloudTag")
+    {
+      scoreVal += 100;
+    }
+  }
+}
---
+++ Assets/FripperController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour
+{
+  //HingeJointコンポーネントを入れる
+  private HingeJoint myHingeJoint;
+
+  //初期の傾き
+  private float defaultAngle = 20;
+  //弾いた時の傾き
+  private float flickAngel = -20;
+
+  // Start is called before the first frame update
+  void Start()
+  {
+    //HingeJoingコンポーネント取得
+    this.myHingeJoint = GetComponent<HingeJoint>();
+
+    //フリッパーの傾きを設定
+    SetAngle(this.defaultAngle);
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+    //左矢印キー or "A"キーを押した時、左フリッパーを動かす
+    if ((Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag") ||
+        (Input.GetKeyDown(KeyCode.A) && tag == "LeftFripperTag"))
+    {
+      SetAngle(this.flickAngel);
+    }
+    //右矢印キー or "D"キーを押した時、右フリッパーを動かす
+    if ((Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag") ||
+        (Input.GetKeyDown(KeyCode.D) && tag == "RightFripperTag"))
+    {
+      SetAngle(this.flickAngel);
+    }
+    //下矢印キー or "S"キーを押したとき、両方のフリッパーを動かす
+    if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
+    {
+      SetAngle(this.flickAngel);
+    }
+
+
+    //矢印キー離された時、フリッパーを元に戻す
+    if ((Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag") ||
+        (Input.GetKeyUp(KeyCode.A) && tag == "LeftFripperTag"))
+    {
+      SetAngle(this.defaultAngle);
+    }
+    if ((Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag") ||
+        (Input.GetKeyUp(KeyCode.D) && tag == "RightFripperTag"))
+    {
+      SetAngle(this.defaultAngle);
+    }
+    if (Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKeyUp(KeyCode.S))
+    {
+      SetAngle(this.defaultAngle);
+    }
+  }
+
+  //フリッパーの傾きを設定
+  public void SetAngle(float angle)
+  {
+    JointSpring jointSpr = this.myHingeJoint.spring;
+    jointSpr.targetPosition = angle;
+    this.myHingeJoint.spring = jointSpr;
+  }
+
+    
+}
